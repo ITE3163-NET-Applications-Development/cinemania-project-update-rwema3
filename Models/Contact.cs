0e9e61dfc3f61894f7c5111ca38892aeb33bf344
rwema3 <52289151+rwema3@users.noreply.github.com>
2021-08-08 03:21:42 +0200

    Add files via upload

---
+++ Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineMania.Models
+{
+    public class Contact
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(2000, ErrorMessage="Message should not exceed 200 characters")]
+        public string Message { get; set; }
+    }
+}
